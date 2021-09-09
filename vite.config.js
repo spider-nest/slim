e232bedd795104bb0af7c6ffdf40a814284d6b40
cnguu <www@cnguu.cn>
2021-09-09 18:00:08 +0800

    feat: caching

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,65 @@
 import path from "path";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import mpa from "vite-plugin-mpa";
 
-export default defineConfig({
-  server: {
-    host: true,
-    port: 9527,
-    strictPort: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ mode }) => {
+  const root = process.cwd();
+  const { VITE_BASE_URL } = loadEnv(mode, root);
+  return defineConfig({
+    server: {
+      host: true,
+      port: 9527,
+      strictPort: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  build: {
-    target: "es2015",
-    outDir: "dist",
-    assetsDir: "assets",
-    assetsInlineLimit: 4096,
-    cssCodeSplit: true,
-    sourcemap: false,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        keep_infinity: true,
-        drop_console: true,
+    build: {
+      target: "es2015",
+      outDir: "dist",
+      assetsDir: "assets",
+      assetsInlineLimit: 4096,
+      cssCodeSplit: true,
+      sourcemap: false,
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          drop_console: true,
+        },
       },
+      brotliSize: true,
+      chunkSizeWarningLimit: 512,
     },
-    brotliSize: true,
-    chunkSizeWarningLimit: 512,
-  },
-  plugins: [
-    svelte(),
-    mpa({
-      open: "/home",
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^@/,
-        replacement: path.resolve(__dirname, "./src"),
-      },
+    root,
+    base: VITE_BASE_URL,
+    plugins: [
+      svelte(),
+      mpa({
+        open: "/home",
+      }),
     ],
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+    resolve: {
+      alias: [
+        {
+          find: /^@/,
+          replacement: path.resolve(__dirname, "./src"),
+        },
+      ],
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
       },
     },
-  },
-});
+  });
+};
