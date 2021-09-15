6d9ec7f7f0b1230980296d9c09f97b10bf05cb16
cnguu <www@cnguu.cn>
2021-09-15 11:54:53 +0800

    feat: caching

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,8 @@
   } from "$configs/app";
 
   import "$styles/routes/index.less";
+
+  let href = "javascript:;";
 </script>
 
 <svelte:head>
@@ -17,10 +19,15 @@
 </svelte:head>
 
 <div class="container">
-  <header class="header"></header>
+  <header class="header">
+    <div class="header__user">
+      统帅大人，请<a href="{href}">登录</a>
+    </div>
+    <div class="header__staff">联系客服</div>
+  </header>
   <main class="main"></main>
   <footer class="footer">
-    <a class="footer__rule" href="javascript:;">活动规则</a>
+    <a class="footer__rule" href="{href}">活动规则</a>
     <div class="footer__copyright">本活动最终解释权归蚁族崛起所有</div>
   </footer>
 </div>
